8eca79be99987aae0d8bdf67c82128776614813d
S P <spatnai3@ncsu.edu>
2022-12-07 04:50:11 -0500

    Starting up: Initial Layers

--- Final_Project_App/server.R
+++ Final_Project_App/server.R
@@ -2,23 +2,89 @@
 # ST 558 Final Project 
 # SMITALI PATNAIK
 #
-
 library(shiny)
+library(shinydashboard)
+library(tidyverse)
+library(readr)
+library(lubridate) 
+library(caret)
+library(DT)
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+# PRIOR WORKS , DATA DOWNLOAD AND FUNCTIONS
 
-    output$distPlot <- renderPlot({
+df1 <-read_delim("Daily_Demand_Forecasting_Orders.csv",delim=";",
+                 show_col_types = FALSE)
+names(df1) <- c('week', 'day', 'non-urgent', 'urgent', 'typeA', 'typeB', 
+                'typeC ', 'fiscal', 'traffic', 'banking1', 'banking2',
+                'banking3', 'target')
 
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+df1<-df1 %>% mutate(day1=as.factor(day))
+df1<-df1 %>% mutate(week1=as.factor(week))
+m<-mean(df1$target)
+df1<-df1%>%mutate(target1=as.factor(ifelse(target<m,0,1)))
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
 
-    })
+df_see_data<-function(df_data,Criteria,nums1=0,nums2=0)
+{
+  if (Criteria==1) {
+    show_df<-df_data 
+    }
+  
+  else if (Criteria==2 )
+    {
+      if(nums1<=nrow(df_data)) 
+        {
+         show_df<-df_data[1:nums1, ] 
+        }
+      else {
+        print("Please select within range")
+         }
+  }
+  else if (Criteria==3 ) 
+      {
+        if(nums2<=ncol(df_data)) 
+        {
+          show_df<-subset(df_data, select=1:nums2)
+        }
+        else {
+          print("Error")
+        }
+  }
+  
+  else if (Criteria==4 ) 
+  {
+    if(nums1<=nrow(df_data) & nums2<=ncol(df_data)) 
+    {
+       show_df1<-df_data[1:nums1, ] 
+       show_df<-subset(show_df1, select=1:nums2)
+    
+    }
+    else {
+      print("Error")
+    }
+  }
+    
+  return (show_df)
+}
 
-})
+
+shinyServer =function(input, output) {
+  output$tab<-renderDataTable({
+    datatable(df_see_data(df1,input$Criteria,input$nums1,input$nums2),
+              extensions = 'Buttons',
+              options = list(
+                paging = TRUE,
+                searching = TRUE,
+                fixedColumns = TRUE,
+                autoWidth = FALSE,
+                ordering = TRUE,
+                dom = 'Bfrtip',
+                scrollX = T,
+                #buttons = c('copy', 'csv', 'excel','pdf'),
+                 buttons=list(list(extend = "excel", text = '<span class="glyphicon glyphicon-th"></span>'), 
+                        list(extend = "csv", text = '<span class="glyphicon glyphicon-download-alt"></span>'))
+                ),
+              class = "display"
+    )  
+  })
+}
--- Final_Project_App/ui.R
+++ Final_Project_App/ui.R
@@ -5,26 +5,68 @@
 
 
 library(shiny)
-
+library(shinydashboard)
+library(tidyverse)
+library(readr)
+library(lubridate) 
+library(caret)
+library(DT)
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
+dashboardPage(skin="black",
+              
+              #TITLE
+              dashboardHeader(title="Supervised Learning Case Study Daily_Demand_Forecasting_Orders",titleWidth=1000),
+              
+              #TABS AS PER INSTRUCTIONS
+              dashboardSidebar(sidebarMenu(
+                menuItem("About", tabName = "about", icon = icon("play-circle", verify_fa = FALSE)),
+                menuItem("Data", tabName = "data", icon = icon("play-circle", verify_fa = FALSE))
+                #menuItem("Data Exploration", tabName = "app", icon= icon("archive",verify_fa = FALSE))
+               # menuItem("Modelling", tabName = "app", icon = icon("archive",verify_fa = FALSE)),
+              )),
+              
+              # BODY 
+              dashboardBody(
+                tabItems(
+                  #TAB1-ABOUT
+                  tabItem(tabName = "about",
+                          fluidRow(
+                            withMathJax(),
+                            column(6,
+                                   h1("Predictions Model using Supervised Machine Learning Methoda"),
+                                   box(background="yellow",width=9,
+                                       h4("This page describes machine learning methods for the data being used"),
+                            ),
+                            
+                            )
+                          )
+                  ),
+                  
+                  #TAB2-DATA 
+                  tabItem(tabName = "data",
+                          fluidRow(
+                  radioButtons("Criteria", label = h5("Select the Filter Type",style = "font-weight:bold"),
+                               choices = list("All" = 1, 
+                                              "Rows" = 2, 
+                                              "Columns" = 3,
+                                               "Select Rows & Columns"=4),
+                               selected = 1),
+                  h4("Selection boxes based on your criteria"),
+                  column(2,
+                  conditionalPanel(
+                    condition = "input.Criteria == '2' | input.Criteria == '4' ",
+                    numericInput("nums1", 'Select number of rows',value=10, min=1,max=100))),
+                  
+                  column(4,
+                  conditionalPanel(
+                    condition = "input.Criteria == '3' |input.Criteria =='4' ",
+                    numericInput("nums2", 'Enter number of columns',value=10, min=1,max=100))),
+                    dataTableOutput("tab", width = 1500) ,
+                                   )
+                            )
+                            
+                          )
+                  )
+                )
+            
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
-    )
-))
