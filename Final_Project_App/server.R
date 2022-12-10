f2367fdb72af75ecfa2349cc8a3ee06d1bebb6a7
S P <spatnai3@ncsu.edu>
2022-12-10 04:54:50 -0500

    updates

--- Final_Project_App/server.R
+++ Final_Project_App/server.R
@@ -9,19 +9,25 @@ library(readr)
 library(lubridate) 
 library(caret)
 library(DT)
-
+library(shinyWidgets)
+library(caTools)
+library(shinyr)
 # PRIOR WORKS , DATA DOWNLOAD AND FUNCTIONS
 
 df1 <-read_delim("Daily_Demand_Forecasting_Orders.csv",delim=";",
                  show_col_types = FALSE)
-names(df1) <- c('week', 'day', 'non-urgent', 'urgent', 'typeA', 'typeB', 
-                'typeC ', 'fiscal', 'traffic', 'banking1', 'banking2',
+names(df1) <- c('week', 'day', 'non_urgent', 'urgent', 'typeA', 'typeB', 
+                'typeC', 'fiscal', 'traffic', 'banking1', 'banking2',
                 'banking3', 'target')
 
 df1<-df1 %>% mutate(day1=as.factor(day))
 df1<-df1 %>% mutate(week1=as.factor(week))
 m<-mean(df1$target)
 df1<-df1%>%mutate(target1=as.factor(ifelse(target<m,0,1)))
+
+
+para <- 1:ncol(df1)
+names(para) <- names(df1)
 
 
 df_see_data<-function(df_data,Criteria,nums1=0,nums2=0)
@@ -67,8 +73,48 @@ df_see_data<-function(df_data,Criteria,nums1=0,nums2=0)
   return (show_df)
 }
 
+all_plots1<-function(df1,ply,ins,outs,cat,catvar)
+{
+  if (ply==1) { #scatter
+    { if (cat==1) 
+      {  ggplot(df1,aes_string(x =ins,
+                                    y = outs))+
+          geom_point(alpha=0.5)+  geom_jitter()
+      }
+      else if(cat==2)
+      {
+          ggplot(df1,aes_string(x =ins,
+                                 y = outs,color=catvar))+
+          geom_point(alpha=0.5)+  geom_jitter() 
+      }
+    }
+  
+  }
+  else if(ply==2){ #bar
+   
+   { if (cat==1) 
+     {  
+       ggplot(df1,aes_string(x =ins,
+                             y = outs))+
+       geom_bar(position='dodge',stat='identity')
+   }
+     else if(cat==2)
+     {
+       ggplot(df1,aes_string(x =ins,
+                              y = outs,color=catvar))+
+       geom_bar(position='dodge',stat='identity') 
+     }
+   }
+   
+  }
+   
+}
 
-shinyServer =function(input, output) {
+  
+
+
+
+shinyServer =function(input,output,session) {
   output$tab<-renderDataTable({
     datatable(df_see_data(df1,input$Criteria,input$nums1,input$nums2),
               extensions = 'Buttons',
@@ -84,7 +130,79 @@ shinyServer =function(input, output) {
                 buttons=list(list(extend = "excel", text = '<span class="glyphicon glyphicon-th"></span>'), 
                              list(extend = "csv", text = '<span class="glyphicon glyphicon-download-alt"></span>'))
               ),
-              class = "display"
-    )  
+              class = "display")
   })
-}
+  
+df2<-reactive({df1})
+ 
+ #trainindex<-reactive ({
+# sample <- sample.split(dfr()$target, SplitRatio = input$prop)
+# dtrain  <- dfr()[sample == TRUE]
+# dtest   <-dfr()[sample == FALSE]
+# }) 
+ 
+#inTrain <- reactive({caret::createDataPartition(df2()$target, p = input$prop, list = FALSE) 
+#train <- df2()[inTrain()]
+#test<- df2()[-inTrain()]
+ #            })  
+lm_model <- reactive({
+      req(df2(),input$columns,'target')
+      x <- as.numeric(df2()[[as.name(input$columns)]])
+      y <- as.numeric(df2()$target)
+      f <- paste0('target', " ~ ", paste0(input$columns, collapse = " + "))
+     f <- as.formula(f)
+      model <- lm(f, data = df2(), na.action=na.exclude)
+      return(model)
+})
+
+
+                           
+
+    output$lmsummary <- renderPrint({
+      req(lm_model())
+      summary(lm_model())
+        })
+        
+     output$importance <- renderPlot({
+     req(lm_model())
+      par(mfrow = c(1,2))
+      plot(lm_model())
+      plot(lm_model())
+      })
+        
+        
+    treeFit <- reactive({
+      req(df2(),'target')
+      x <- as.numeric(df2()[[as.name(names(df2()))]])
+      y <- as.numeric(df2()$target)
+      f <- paste0('target', " ~ ", "." )
+      f <- as.formula(f)
+      train(f, data=df2(), method = "rpart",
+                       trControl = trainControl(method = "repeatedcv", number = input$folds, repeats = input$rep),
+                        preProcess = c("center", "scale"),
+                        tuneGrid = data.frame(cp = seq(0, 0.1, 0.001)))
+          
+    return(treeFit)
+})
+  
+output$rtreesummary <- renderPrint({
+  req(treeFit())
+  summary(treeFit())
+}) 
+  
+    
+}   
+
+  
+ 
+ 
+  
+  
+  
+  
+  
+
+  
+  
+  
+  
--- Final_Project_App/ui.R
+++ Final_Project_App/ui.R
@@ -11,19 +11,24 @@ library(readr)
 library(lubridate) 
 library(caret)
 library(DT)
+library(shinyWidgets)
+library(shinyWidgets)
+library(caTools)
+
+para <- 1:ncol(df1)
+names(para) <- names(df1)
 
 
 dashboardPage(skin="black",
-              
               #TITLE
               dashboardHeader(title="Supervised Learning Case Study Daily_Demand_Forecasting_Orders",titleWidth=1000),
               
               #TABS AS PER INSTRUCTIONS
               dashboardSidebar(sidebarMenu(
                 menuItem("About", tabName = "about", icon = icon("play-circle", verify_fa = FALSE)),
-                menuItem("Data", tabName = "data", icon = icon("play-circle", verify_fa = FALSE))
-                #menuItem("Data Exploration", tabName = "app", icon= icon("archive",verify_fa = FALSE))
-                # menuItem("Modelling", tabName = "app", icon = icon("archive",verify_fa = FALSE)),
+                menuItem("Data", tabName = "data", icon = icon("play-circle", verify_fa = FALSE)),
+                menuItem("Data Exploration", tabName = "dataexploration", icon= icon("play-circle",verify_fa = FALSE)),
+                menuItem("Modelling", tabName = "modelling", icon = icon("play-circle",verify_fa = FALSE))
               )),
               
               # BODY 
@@ -42,7 +47,7 @@ dashboardPage(skin="black",
                             )
                           )
                   ),
-                  
+
                   #TAB2-DATA 
                   tabItem(tabName = "data",
                           fluidRow(
@@ -64,9 +69,82 @@ dashboardPage(skin="black",
                                      numericInput("nums2", 'Enter number of columns',value=10, min=1,max=100))),
                             dataTableOutput("tab", width = 1500) ,
                           )
-                  )
-                )
-              )
-)
-
+                  ),
+                  tabItem(tabName = "dataexploration",
+                          fluidRow(
+                            column(5,                                                 
+                                   box(width=50,
+                                       title="Scatter Plot",
+                                       background="black",
+                                       solidHeader=TRUE,
+                                        selectInput("ply", "Select Type of plot",
+                                                       choices=list("Scatter"=1,"Bar"=2, selected = NULL)),
+                                         conditionalPanel(
+                                          condition = "input.ply == '1' | input.ply == '2' ",
+                                         
+                                           selectInput("ins", "Select Variable for X",
+                                                       c("week","day","non_urgent","urgent","typeA",
+                                                         "typeB","typeC","fiscal","traffic" ,"banking1",
+                                                         "banking2" ,"banking3","target","day1","week1" ,"target1")),
+                                           selectInput("outs", "Select Variable for Y",
+                                                       c("week","day","non_urgent","urgent","typeA",
+                                                         "typeB","typeC","fiscal","traffic" ,"banking1",
+                                                         "banking2" ,"banking3","target","day1","week1" ,"target1")),
+                                           radioButtons("cat", label = h5("Plot Category wise?",style = "font-weight:bold"),
+                                                        choices = list("No" = 1, 
+                                                                       "Yes" = 2), selected = 1),
+                                           h4("Select Category variable to plot"),
+                                           conditionalPanel(
+                                             condition = "input.cat == '1'",
+                                             selectInput("catvar", "Select Variable for Category",
+                                                         c("day1","week1" ,"target1"))),
+                                         ),
+                                             br(),
+                                             br(),
+                                             plotOutput("plot1")
+                                         
+                                       
+                                   )))),
+                  
+                  tabItem(tabName = "modelling",
+                          fluidRow(
+                                   tabBox(
+                                   title="Split, Modelling and Predictions ",
+                                   #tabsetPanel("Modelling inf",
+                                      tabPanel("Modelling Info","text")),
+                                   
+                                      tabPanel("Regression Fits",
+                                            selectInput("columns", "Select Variable for Y",
+                                                          c("week","day","non_urgent","urgent","typeA",
+                                                            "typeB","typeC","fiscal","traffic" ,"banking1",
+                                                            "banking2" ,"banking3","target","day1","week1" ,"target1"),multiple=TRUE),
+                                              #sliderInput(
+                                               # "prop",
+                                               # label = h3("Use this to get Train/Test Split %"),
+                                               # min = 0,
+                                               # step=0.1,
+                                               # max = 1,
+                                              #  value =0.75),
+                                              numericInput("prop", 'Select percentage',value=0, min=1,step=1),
+                                               br(),
+                                               br(),
+                                               #textOutput('result'),
+                                               ##DT::dataTableOutput('dftable'),
+                                               h3("Multiple Linear Regression"),
+                                               verbatimTextOutput('lmsummary'),
+                                               plotOutput('importance')),
+                                    tabPanel(
+                                      "Decision Tree",
+                                     numericInput("fold", 'Select percentage',value=3, min=1,step=5),
+                                     numericInput("rep", 'Select percentage',value=3, min=1,step=5),
+                                     verbatimTextOutput('rtreesummary'),
+                                      ))))
+                                   
+                                   
+                                   ))
+                                  
+                  
+                  
+                  
+               
 
